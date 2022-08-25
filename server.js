b8464c57d97787afa0b5c6d9c345fbba21c6aba2
Takuma Sato <kanikama038@gmail.com>
2022-08-25 14:22:04 +0900

    init

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const socket = require("socket.io");
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end(fs.readFileSync("./index.html"));
-}).listen(8000 || process.env.PORT);
+}).listen(process.env.PORT || 8000);
 
 const io = socket(server);
 
