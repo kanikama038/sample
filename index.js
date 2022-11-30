171c5743462d118cec9acd48bad47987791fcd9a
Takuma Sato <kanikama038@gmail.com>
2022-11-30 11:42:18 +0900

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+let http = require("http");
+let fs = require("fs");
+let socket = require("socket.io");
+let server = http.createServer((res, req)=>{
+    if (req.url=="/"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(fs.readFileSync("./index.html"));
+    }
+}).listen(process.env.PORT || 8000);
+let io = socket(server);
